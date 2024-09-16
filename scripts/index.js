76b64ba7f960d127e1cf137fce12ebec88b4a335
Salhi Rayen <rayen.rsalhi9@gmail.com>
2024-09-16 17:10:28 +0100

    created menu page.

--- scripts/index.js
+++ scripts/index.js
@@ -1,23 +1,3 @@
-const iconContainer = document.querySelector('.dark-mode-container');
-const icon = document.querySelector('svg');
+import { handleDarkMode } from "./utils/darkMode.js";
 
-icon.addEventListener('click', () => {
-
-    if (icon.classList.contains('clicked') && iconContainer.classList.contains('clicked')) {
-
-        icon.classList.remove('clicked');
-        iconContainer.classList.remove('clicked');
-
-        document.documentElement.style.setProperty('--dark-color', '#111111');
-        document.documentElement.style.setProperty('--light-color', '#fafafa');
-
-    } else {
-
-        icon.classList.add('clicked');
-        iconContainer.classList.add('clicked');
-
-        document.documentElement.style.setProperty('--dark-color', '#fafafa');
-        document.documentElement.style.setProperty('--light-color', '#111111');
-
-    }
-});
+handleDarkMode();
---
+++ scripts/utils/darkMode.js
@@ -0,0 +1,25 @@
+export function handleDarkMode() {
+    const iconContainer = document.querySelector('.dark-mode-container');
+    const icon = document.querySelector('svg');
+
+    icon.addEventListener('click', () => {
+
+        if (icon.classList.contains('clicked') && iconContainer.classList.contains('clicked')) {
+
+            icon.classList.remove('clicked');
+            iconContainer.classList.remove('clicked');
+
+            document.documentElement.style.setProperty('--dark-color', '#111111');
+            document.documentElement.style.setProperty('--light-color', '#fafafa');
+
+        } else {
+
+            icon.classList.add('clicked');
+            iconContainer.classList.add('clicked');
+
+            document.documentElement.style.setProperty('--dark-color', '#fafafa');
+            document.documentElement.style.setProperty('--light-color', '#111111');
+
+        }
+    });
+}
