2a8f48ae5c788df613660152e77632c4fed243f5
Salhi Rayen <rayen.rsalhi9@gmail.com>
2024-09-16 18:43:44 +0100

    created spending section, still to work on JS.

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,3 +1,10 @@
 import {handleDarkMode} from './utils/darkMode.js';
 
 handleDarkMode();
+
+const options = document.querySelectorAll('.option');
+options.forEach(option => {
+    option.addEventListener('click', () => {
+        window.location.href = `${option.dataset.link}`;
+    });
+});
