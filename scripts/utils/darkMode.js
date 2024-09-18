ea87a1df5db7e1344f54db1c9dcebd58786ba2e7
Salhi Rayen <rayen.rsalhi9@gmail.com>
2024-09-18 15:19:34 +0100

    enhanced dark mode colors.

--- scripts/utils/darkMode.js
+++ scripts/utils/darkMode.js
@@ -10,14 +10,14 @@ export function handleDarkMode() {
             iconContainer.classList.remove('clicked');
 
             document.documentElement.style.setProperty('--dark-color', '#111111');
-            document.documentElement.style.setProperty('--light-color', '#fafafa');
+            document.documentElement.style.setProperty('--light-color', '#faf5e9');
 
         } else {
 
             icon.classList.add('clicked');
             iconContainer.classList.add('clicked');
 
-            document.documentElement.style.setProperty('--dark-color', '#fafafa');
+            document.documentElement.style.setProperty('--dark-color', '#faf5e9');
             document.documentElement.style.setProperty('--light-color', '#111111');
 
         }
