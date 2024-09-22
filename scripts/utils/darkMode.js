34d14ec98e3838f583224358fb3ff0ea837fda2c
Salhi Rayen <rayen.rsalhi9@gmail.com>
2024-09-22 12:04:52 +0100

    tested darkMode function : initialized appearance right.

--- scripts/utils/darkMode.js
+++ scripts/utils/darkMode.js
@@ -1,25 +1,40 @@
+export let darkModeOn = JSON.parse(localStorage.getItem('darkMode')) || false;
+
 export function handleDarkMode() {
+    
     const iconContainer = document.querySelector('.dark-mode-container');
     const icon = document.querySelector('svg.icon');
 
+    initializeAppearance();
+
     icon.addEventListener('click', () => {
 
-        if (icon.classList.contains('clicked') && iconContainer.classList.contains('clicked')) {
+        darkModeOn = !darkModeOn;
+        localStorage.setItem('darkMode', JSON.stringify(darkModeOn));
 
-            icon.classList.remove('clicked');
-            iconContainer.classList.remove('clicked');
+        initializeAppearance();
+    });
 
-            document.documentElement.style.setProperty('--dark-color', '#111111');
-            document.documentElement.style.setProperty('--light-color', '#faf5e9');
+    function initializeAppearance() {
 
-        } else {
+        if (darkModeOn === true) {
 
             icon.classList.add('clicked');
             iconContainer.classList.add('clicked');
-
+    
             document.documentElement.style.setProperty('--dark-color', '#faf5e9');
             document.documentElement.style.setProperty('--light-color', '#111111');
 
+        } else {
+
+            icon.classList.remove('clicked');
+            iconContainer.classList.remove('clicked');
+    
+            document.documentElement.style.setProperty('--dark-color', '#111111');
+            document.documentElement.style.setProperty('--light-color', '#faf5e9');
+
         }
-    });
+    
+    }
+    
 }
